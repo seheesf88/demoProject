282db287785730fc9ede92821a4b2457b72e012a
seheesf88 <seheesf88@gmail.com>
2019-07-15 20:35:06 -0700

    singup added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.Promise = global.Promise;
 
@@ -30,6 +31,7 @@ app.use((req, res, next) => {
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 
 mongoose.connect(
